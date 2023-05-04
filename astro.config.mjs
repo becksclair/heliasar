7cdc3e94479236fa4e81b033828afd35eee81e40
Rebecca Clair <rebecca@heliasar.com>
2023-05-04 18:16:03 +0200

    Added astro-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import partytown from "@astrojs/partytown";
 import compress from "astro-compress";
-
 import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://heliasar.com",
-
-	integrations: [solidJs(), partytown(), compress(), robotsTxt()],
+  site: "https://heliasar.com",
+  integrations: [solidJs(), partytown(), compress(), robotsTxt(), sitemap()]
 });
