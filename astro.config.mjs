33cf06668765ffed0c54fa229fbde4a41c4f2b44
Rebecca Clair <rebecca@heliasar.com>
2023-05-04 18:44:28 +0200

    Add vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import solidJs from "@astrojs/solid-js";
 import partytown from "@astrojs/partytown";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://heliasar.com",
-  integrations: [solidJs(), partytown(), compress(), robotsTxt(), sitemap()]
+  integrations: [solidJs(), partytown(), compress(), robotsTxt(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
