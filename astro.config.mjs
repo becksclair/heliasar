67b51f2161de8d09beb595b5b4cc9db575a72bd9
Rebecca Clair <rebecca@heliasar.com>
2023-05-04 18:09:58 +0200

    Added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import partytown from "@astrojs/partytown";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), partytown()]
+  integrations: [solidJs(), partytown(), compress()]
 });
