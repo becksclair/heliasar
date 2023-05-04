60bea1adeeed82dc87604acac204176a6711773f
Rebecca Clair <rebecca@heliasar.com>
2023-05-04 18:13:19 +0200

    Added astro-robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import partytown from "@astrojs/partytown";
-
 import compress from "astro-compress";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), partytown(), compress()]
+	site: "https://heliasar.com",
+
+	integrations: [solidJs(), partytown(), compress(), robotsTxt()],
 });
