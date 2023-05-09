910cd4bd213345834f4c98fd82f304f4185960ee
Rebecca Clair <rebecca@heliasar.com>
2023-05-09 20:17:06 +0200

    Build as static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/edge";
+// import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://rbclair.me",
 	integrations: [solidJs(), partytown(), compress(), robotsTxt(), sitemap()],
-	output: "server",
-	adapter: vercel(),
+	output: "static",
+	// adapter: vercel(),
 });
